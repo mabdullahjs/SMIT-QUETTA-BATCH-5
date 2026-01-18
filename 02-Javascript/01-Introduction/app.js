adf827aaee67579fdbd3c1a0b9980cd0d70013e6
mabdullahjs <mabdullah2037@outlook.com>
2026-01-18 15:12:26 +0500

    js started

---
+++ 02-Javascript/01-Introduction/app.js
@@ -0,0 +1,67 @@
+// debugging
+
+// console.log("Hello world");
+// console.log("Hello Abdullah");
+
+
+// Data types
+
+// string "dsafsdf123!@#!@$#!" 'fsafsdf' '12'
+// number 123 12.3
+// boolean true false
+
+// object
+// symbol
+// bigint
+// undefined
+// null
+
+
+
+// console.log("hello")
+// console.log(123)
+// console.log(true)
+
+
+
+
+
+// () function
+// alert("hello world 1")
+// alert("hello world 2")
+// alert("hello world 3")
+
+
+
+// console.log("hello world ");
+
+// variable ka matlb container koi bhi data container ka undar store krwado jab jab us container ko bulaoga data appka pass aajayega.
+
+
+// var username = "Muhammad Abdullah Khan";
+// console.log(username)
+
+// concatination
+
+// var $_firstName123 = "Muhammad";
+// var firstName = "Muhammad";
+// var lastName = "Abdullah Khan"
+// var age = 22;
+// console.log(10 + "10");
+
+// var firstName = prompt("enter your firstname");
+// var lastName = prompt("enter your lastname")
+// var age = prompt("enter your age");
+
+// console.log("user full name is " +firstName + " " + lastName + " and user age is " + age);
+
+
+
+
+// var username = "abdullah"; // initialize, banadia
+// console.log(username);
+// username = "Ammar" // reassign
+// console.log(username);
+// var username = "Usman"; //redeclare
+// console.log(username);
+
