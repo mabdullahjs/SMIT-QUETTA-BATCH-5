dbcb4d3553d0896bc3ca0783ef1bea5e2d3a6326
mabdullahjs <mabdullah2037@outlook.com>
2026-02-28 16:03:03 +0500

    function started

---
+++ 02-Javascript/03-Conditional-statement/condition.js
@@ -0,0 +1,103 @@
+// console.log("hello if else");
+
+// 18 > usko admission milaiga gym ma : admission nahi milaiga
+
+// let age = 15;
+
+// if(age >= 18){
+//     console.log("Gym ma ajao...");
+
+// } else {
+//     console.log("No admission");
+
+// }
+
+// let age = 8;
+
+// if (age >= 18) {
+//   console.log("Gym ma ajao...");
+// } else if (age >= 15) {
+//   console.log("1000 zyada do or ajao.");
+// } else {
+//   console.log("No admission");
+// }
+
+// let bankBalance = 2000001;
+
+// if (bankBalance >= 20000000) {
+//   console.log("Kal hi ajaaao!");
+// } else if (bankBalance >= 2000000) {
+//   console.log("Shaaaadi Mubarak!");
+// } else {
+//   console.log(
+//     "Ammi na zehr kahlia. Istekhara ma naa ayaa ha. Abhi waqt ha abhi bari bhen ki shaadi honi ha. Ma sabsa choti hon. ",
+//   );
+// }
+
+
+
+
+// let speed = 10;
+
+// if(speed < 30){
+//     console.log("Too slow");
+    
+// } else if(speed >= 30 && speed <= 60){
+//     console.log("Safe speed");
+    
+// } else if(speed >= 61 && speed <= 100){
+//     console.log("Speeding");
+    
+// } else {
+//     console.log("Dangerously fast");
+    
+// }
+
+
+
+// let returnDay = 15;
+
+// if(returnDay === 0){
+//     console.log("$0 fine");
+    
+// } else if(returnDay <= 5){
+//     console.log("$1 fine per day " , returnDay * 1);
+    
+// } else if(returnDay <= 10){
+//     console.log("$2 fine per day " , returnDay * 2);
+    
+// } else {
+//     console.log("$5 fine per day " , returnDay * 5);
+
+// }
+
+
+// let age = 8;
+// let ticket = true
+
+// if(age >= 18 && ticket){
+//     console.log("welcome to my event");
+    
+// } else {
+//     console.log("chalo shaba katto");
+    
+// }
+
+
+
+
+// let time = 17;
+
+// if(time >= 6 && time <= 10){
+//     console.log("Naan Channa");
+    
+// } else if(time >= 12 && time <= 14){
+//     console.log("Chicken Biryani");
+    
+// } else if(time >= 18 && time <= 21){
+//     console.log("Chicken paneer tikka");
+    
+// } else {
+//     console.log("Snack");
+
+// }
---
+++ 02-Javascript/04-Functions-&-DOM/app.js
@@ -0,0 +1,34 @@
+// block / scope
+// function reuseable() {
+//   console.log("hello world 1");
+//   console.log("hello world 2");
+//   console.log("hello world 3");
+//   console.log("hello world 4");
+//   console.log("hello world 5");
+// }
+
+// reuseable()
+// reuseable()
+
+// parameter
+// arguments
+
+// function greetUser(username , age){
+//     console.log("Hello " + username + " with age " + age);
+    
+// }
+
+// greetUser("abdullah" , 21)
+// greetUser("usman" , 22)
+// greetUser("ammar" , 18)
+// greetUser("affan" , 18)
+
+
+// function total(num1 , num2){
+//     console.log( "total ===> " , num1 + num2);
+    
+// }
+
+// total(20 , 30)
+// total(69 , 39)
+
