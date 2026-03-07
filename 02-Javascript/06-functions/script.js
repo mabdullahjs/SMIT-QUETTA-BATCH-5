94f651d2010c46087feb230cee517e93581fc14e
mabdullahjs <mabdullah2037@outlook.com>
2026-03-07 15:19:11 +0500

    dom manipulation started

---
+++ 02-Javascript/06-functions/script.js
@@ -0,0 +1,41 @@
+// function greet() {
+//   console.log("Hello world 1");
+//   console.log("Hello world 2");
+//   console.log("Hello world 3");
+//   console.log("Hello world 4");
+//   console.log("Hello world 5");
+// }
+
+
+// greet();
+// greet();
+
+
+// function sum(a , b){
+//     console.log(a+b)
+// }
+
+// sum( 10 , 20)
+
+// sum(12 , 17)
+
+// function greet(name){
+//     console.log("Hello " , name )
+// }
+
+// greet("Rizwan")
+
+// greet("salam")
+
+function sum(a , b){
+    return a + b
+    console.log("after return")
+    console.log("after return")
+    console.log("after return")
+    console.log("after return")
+    console.log("after return")
+
+}
+
+
+console.log(sum(5 , 4))
---
+++ 02-Javascript/07-DOM/app.js
@@ -0,0 +1,120 @@
+// let container = document.querySelector(".container");
+// let container = document.querySelector("#container");
+
+// let container = document.getElementById('container')
+
+// container.innerHTML = '<h1> Hello </h1>'
+
+// container.style.backgroundColor = 'blue'
+
+// let heading = document.querySelector('.heading');
+
+// heading.innerHTML = 'Hello from javaScript';
+
+// heading.style.color = 'white';
+// heading.style.backgroundColor = 'black';
+// heading.style.padding = '10px'
+// heading.style.display = 'inline'
+
+// argument
+// parameter
+
+// function greetUser (username){
+//     console.log('hello ' + username);
+
+// }
+
+// greetUser("Abdullah")
+// greetUser("Usman")
+
+// return
+
+// undefined | null
+// let age = null;
+// console.log(age);
+
+// function greetings(username){
+//     console.log("hello " + username);
+//     return "hello"
+// }
+
+// greetings()
+// // let username = greetings("abdullah");
+// // console.log(username);
+// console.log(greetings("Usman"));
+
+// greetings("abdullah")
+// let username = prompt("enter your name");
+// console.log(username);
+
+// return ka matlb jaha function call hoga waha return hui wi value aayegi.
+
+// function greetUser(){
+//    return "hello abdullah"
+
+// }
+
+// console.log(greetUser());
+
+// function total(num1 , num2){
+//     return num1 + num2
+// }
+
+// console.log(total(20 , 30));
+
+// function cube(num) {
+//   return num ** 3;
+// }
+
+// console.log(cube(10));
+
+// function average(num1, num2, num3) {
+//     let sum = num1 + num2 + num3
+//     return sum / 3
+// }
+
+// console.log(average(100,100,90));
+
+
+
+
+// function combine(val1 , val2){
+//     console.log(typeof(val1) , typeof(val2));
+//     if(typeof(val1) === "number" && typeof(val2) === "number"){
+//         console.log("Sum of numbers " , val1 + val2);
+        
+//     }else {
+//         console.log("string concatination " , val1 + val2);
+        
+//     }
+    
+// }
+
+// combine(20 , 21)
+
+
+// recurssion
+
+
+// DOM manipulation
+
+
+// let const
+
+const h1 = document.querySelector("#head-one")
+const para = document.querySelector("#para-one")
+// h1.innerHTML = "Changed through js"
+// console.log(h1.innerHTML);
+
+
+
+
+
+function changetext(){
+    console.log("text change kronga isma");
+
+    h1.innerHTML = "Changed through js"
+    para.innerHTML = "changed through javascript. Babar azam is the best batsman in the world. Pakistan will win t20 world cup 3027. Agha jee 28 wala match chahya"
+    
+}
+
