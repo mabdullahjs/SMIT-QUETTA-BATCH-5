3ccf4d215bb194b46d163f8324a4e8f5e1c7c894
mabdullahjs <mabdullah2037@outlook.com>
2026-03-28 15:41:15 +0500

    loops

---
+++ 02-Javascript/08-Loops/script.js
@@ -0,0 +1,118 @@
+// For  Loop
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i] + 2)
+// }
+
+// let guests = ['Ali', 'rehman', 'shahzaib', 'Rizwan'];
+
+// for (let i = 0; i < guests.length; i++) {
+//     if(i == 0 ){
+//         console.log('You are not invited')
+//     }else{
+//         console.log('You are invited')
+//     }
+// }
+
+// while loop
+
+// let i = 0;
+// while (i  < 0) {
+//   console.log(i);
+//   i++;
+// }
+
+// // do while loop
+
+// let index = 0;
+
+// do {
+//   console.log('xyz');
+//   i++ ;
+// } while (index < 0 );
+
+
+
+// for(let i = 1 ; i <= 10 ; i++){
+//   console.log(`2 x ${i} = ${2*i}`)
+// }
+
+
+
+// console.log("hello world");
+
+
+
+// for / while / dowhile
+
+// initialization
+// condition
+// increment
+
+// iteration
+
+// for(let i = 0; i < 10; i++){
+//   console.log("hello world from loop" , i);
+  
+// }
+
+// backtick
+
+// let username = "Muhammad Abdullah";
+// let age = 22
+// let city = "karachi";
+
+// "user name is {username} and user age is {age} and user live in {city}"
+
+// console.log("user name is "+ username +  " and user age is " + age + " and user live in " + city);
+
+// console.log(`username is ${username} and user age is ${age} and user live in ${city}`);
+
+// let num = 78
+
+// for(let i = 1; i <= 10; i++){
+//   console.log(`${num} x ${i} = ${num*i}`);
+  
+// }
+
+
+
+// infinite loop 
+
+// for(let i = 10; i > 1; i++)
+
+
+// nested loop
+
+
+// for(let i = 1; i <= 5; i++){
+//   console.log(`outer loop ${i}`);
+
+//   for(let j = 1; j <= 10; j++){
+//     console.log(`inner loop ${j}`);
+    
+//   }
+  
+// }
+
+
+
+
+// for(let i = 1; i <= 5000; i++){
+//   console.log(`Table of ${i}`);
+
+//   for(let j = 1; j <= 10; j++){
+//     console.log(`${i} * ${j} = ${i * j}`);
+    
+//   }
+  
+// }
+
+
+
+
+
+// console.log("console after loop");
+
+// let guests = ['Ali', 'rehman', 'shahzaib', 'Rizwan'];
