538caf3520b51123555ec0ee978ea9d4c554205b
mabdullahjs <mabdullah2037@outlook.com>
2026-04-11 18:01:21 +0500

    array methods

---
+++ 02-Javascript/11-revision/app.js
@@ -0,0 +1,178 @@
+// console.log("hello world");
+
+// string "asdsad" '123@#$@$asdasd'
+// number 20 20.42
+// boolean true / false
+
+// object
+// undefined
+// null
+// bigint
+
+// console.log("hello world");
+// console.log(20);
+// console.log(true);
+
+// let username = "Abdullah"; // initialize
+// username = "Usman" // reassign
+// console.log(username)
+
+// const username = "Abdullah";
+// console.log(username)
+
+// let $_username12 =  "Abdullah"
+
+// var / let / const
+
+// let num1 = 10;
+// let num2 = '20';
+
+// // concatination
+
+// console.log(num1 + num2);
+// +-*/%
+
+// console.log("Abdullah " + 20 + " khan" + true);
+
+// assignment operators
+// let num = 10
+// num += 20;
+// num = num + 20
+// console.log(num);
+
+// comparision operator
+
+// revision
+
+// logical operator
+
+// && ||
+
+// true && false && true
+// true && false || true
+// false || true
+// true
+
+// conditional statement (if / else)
+
+// let age = 18;
+
+// if(age >= 18){
+//     console.log("Welcome to my gym");
+
+// }else {
+//     console.log("You are not eligible");
+
+// }
+
+// let age = 19;
+
+// if (age >= 18) {
+//   console.log("Welcome to my gym");
+// } else if (age >= 15 && age < 18) {
+//   console.log("please pay 1000 extra");
+// } else {
+//   console.log("You are not allowed");
+// }
+
+// function
+
+// function greet(name , age){
+//     console.log("My name is" + " " + name + " age is " + age);
+// }
+
+// greet("Abdullah" , 21)
+// greet("Ali" , 22)
+
+// const title = document.querySelector("#Title");
+// title.innerHTML = "Hello DOM";
+
+// title.style.fontSize = "50px";
+// title.style.padding = "30px"
+
+// const para = document.querySelector(".para");
+// para.innerHTML = "this para is changed!"
+
+// function ChangeColor(){
+//     const title = document.querySelector("#Title");
+//     title.style.color = "yellow";
+//     title.style.backgroundColor = "Black";
+// }
+
+// function submit(){
+//     const input = document.querySelector("#input");
+//     console.log(input.value);
+//     input.value = ""
+// }
+
+// arrays
+
+// let name = 'ibrahim'
+
+// let arr =['abdul' , 12 ,true ];
+
+// console.log(arr[2])
+// console.log(arr.length);
+
+// arr.push('salam')
+// console.log(arr)
+
+// arr.pop()
+
+// console.log(arr)
+
+// arr.unshift(12
+// )
+// console.log(arr)
+
+// arr.shift()
+
+// console.log(arr)
+
+// arr.splice(0 , 1 , 'SMIT' , "Abdullah")
+// console.log(arr)
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(numbers.indexOf(5))
+
+// console.log(numbers.reverse());
+
+let num = [3, 6, 3, 1, 8, 9];
+
+// console.log(num.sort())
+
+let copy = numbers.slice(1 , 5)
+
+// console.log(copy)
+
+// console.log(numbers.join('%'))
+
+
+let a = [1, 2,3]
+let b = [4 ,5 ,6]
+
+// let c = a.concat(b)
+
+// console.log(a+b)
+
+let str1 = 'abdul'
+let str2 = 'salam'
+
+console.log(22 + 22)
+
+console.log(str1 + str2)
+
+console.log(a.concat(b))
+
+//push
+//pop
+//unshift 
+//shift
+//slice 
+//splice
+//reverse
+//sort 
+// indexof
+//concat
+//join
