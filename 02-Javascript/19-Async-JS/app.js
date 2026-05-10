65c23a6aa2bc717e04bdd417921c25fb3e6a0532
mabdullahjs <mabdullah2037@outlook.com>
2026-05-10 16:05:01 +0500

    api call

--- 02-Javascript/19-Async-JS/app.js
+++ 02-Javascript/19-Async-JS/app.js
@@ -43,31 +43,31 @@ console.log("hello world");
 
 // })
 
-function shaadiScnz(bankBalance){
-    return new Promise((resolve,  reject)=>{
-        setTimeout(() => {
-            if(bankBalance > 5000000){
-                resolve("shaadi Mubarak")
-            } else {
-                reject("Istekhara ma naa aya ha..")
-            }
-        }, 2000);
-    })
+// function shaadiScnz(bankBalance){
+//     return new Promise((resolve,  reject)=>{
+//         setTimeout(() => {
+//             if(bankBalance > 5000000){
+//                 resolve("shaadi Mubarak")
+//             } else {
+//                 reject("Istekhara ma naa aya ha..")
+//             }
+//         }, 2000);
+//     })
 
-}
+// }
 
-const executePromise = async ()=>{
-    try {
-        const result = await shaadiScnz(200000)
-        console.log(result);
-    } catch (error) {
-        console.error(error);
+// const executePromise = async ()=>{
+//     try {
+//         const result = await shaadiScnz(200000)
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
         
-    }
+//     }
     
-}
+// }
 
-executePromise()
+// executePromise()
 
 // shaadiScnz(50000000)
 // .then((res)=>{
@@ -96,22 +96,78 @@ executePromise()
 // console.log("console after alert");
 
 
+//JSON
+// javascript object notation
+
+// {
+//     "name":"abdullah"
+// }
+// const div = document.querySelector(".container");
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
+// .then(res => {
+//     console.log(res);
+//     res.map(item => {
+//         div.innerHTML += `<div class="children">
+//             <h2>Name: ${item.name}</h2>
+//         </div>`
+//     })
+    
+// })
+// .catch(err =>{ 
+//     console.log(err);
+    
+// })
 
 
 
 
 
+// const div = document.querySelector(".container");
+
+// fetch('https://fakestoreapi.com/products')
+// .then(res => res.json())
+// .then(res => {
+//     console.log(res);
+//     res.map(item => {
+//         div.innerHTML += `<div class="card">
+//             <img src=${item.image} alt="product image">
+//             <h2>${item.title}</h2>
+//             <p>Price: $${item.price}}</p>
+//             <p>${item.description}</p>
+//         </div>`
+//     })
+    
+// }).catch(err =>{
+//     console.log(err);
+    
+// })
 
 
 
 
 
+// https://weather-322.web.app/
+const apikey = 'your_api_key'
+// https://www.weatherapi.com/
+// `https://api.weatherapi.com/v1/current.json?key=${key}&q=${cityname}&aqi=no`
 
 
 
+// const input = document.querySelector("#input");
+// const btn = document.querySelector("#btn");
 
-
-
+// btn.addEventListener('click' , ()=>{
+//     fetch(`https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${input.value}&aqi=no`)
+//     .then(res =>res.json())
+//     .then(res => {
+//         console.log(res);
+        
+//     }).catch(err => {
+//         console.log(err);
+        
+//     })
+// })
 
 // settimeout
 // setinterval
@@ -119,5 +175,4 @@ executePromise()
 // then catch
 // async await
 // fetch
-// axios
 // advance
